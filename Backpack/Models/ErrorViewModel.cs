1b4c9a1ffb98e4ed20336bb2811495b7ceb7ca43
MVoronchikhin <mvoronchikhin@poidem.ru>
2019-04-05 23:12:14 +0700

    Добавьте файлы проекта.

---
+++ Backpack/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Backpack.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
